4f04cbcf6384ebd6f9aede9764aa5c0fc105b55b
unknown <yccozy2001@163.com>
2022-05-18 19:19:54 +0800

    update shell to use cmdline

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,31 @@
-#include "test.hpp"
+#include "shell_input.h"
+#include "Lex.h"
 #include "AST.h"
 
-int main() {
+#include <string>
+#include <iostream>
 
-    TNP token_head = get_token();
-    print_all_tokens(token_head);
+int main(int argc, char** argv) {
 
-    ProgramAST program(next(token_head));
+    std::string input_filename, output_filename, debug_mode, optimizer;
+    bool to_assembly;
+    shell_input(argc, argv, input_filename, output_filename, debug_mode, optimizer, to_assembly);
+    if (input_filename.empty()) return 0;
+    if (debug_mode == "shell")
+        std::cout << "input: " << input_filename << "\noutput: " << output_filename << "\ndebug: " << debug_mode
+                  << "\nwhether to assembly = " << to_assembly << "\nwhich optimizer = " << optimizer << std::endl;
+
+    Lexical program_file(input_filename);
+    program_file.get_token();
+    TNP token_head = program_file.head;
+    if (debug_mode == "lex")
+        print_all_tokens(token_head);
+
+    ProgramAST program(token_head);
     program.Parse();
     ANP AST_head = program.head;
-    print_all_ASTs(AST_head);
+    if (debug_mode == "parse")
+        print_all_ASTs(AST_head);
 
     return 0;
 } 
