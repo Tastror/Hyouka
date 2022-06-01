c459622043579eb94b99de5f056b88e2e5aef29f
SimonWXW <wuxiwen@mail.nwpu.edu.cn>
2022-06-02 02:24:46 +0800

    backend framework

---
+++ source/backend/Define/define.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Simon on 2022/6/2.
+//
+
+//TODO: control flow graph implementation
---
+++ source/backend/Define/include/define.h
@@ -0,0 +1,7 @@
+//
+// Created by Simon on 2022/6/2.
+//
+
+#pragma once
+
+//TODO: control flow graph define
