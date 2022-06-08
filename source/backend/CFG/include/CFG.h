351afe8de2756280bcf9990d0268192b51ca0436
SimonWXW <wuxiwen@mail.nwpu.edu.cn>
2022-06-09 02:23:40 +0800

    cfg 10%

--- source/backend/CFG/include/CFG.h
+++ source/backend/CFG/include/CFG.h
@@ -6,9 +6,36 @@
 
 #include "backend_define.h"
 
+class BB{
+public:
+    BB_PTR head;
+    BB_PTR now_bb;
+    IR_PTR IR;
+    static int line_num;
+    static int label_num;
+    static int now_register;
+
+    explicit BB(const std::shared_ptr<IR_node>& IR_head);
+
+    void Generate();
+
+    void basic_generate(const std::shared_ptr<IR_node>& now_IR);
+};
+
 class CFG{
 public:
     CFG_PTR head;
+    CFG_PTR now_cfg;
+    BB_PTR BB;
+    static int line_num;
+    static int label_num;
+    static int now_register;
+
+    explicit CFG(const std::shared_ptr<BB_node>& BB_head);
 
     void Generate();
+
+    void entry_generate(const std::shared_ptr<BB_node>& now_BB);
+    void basic_generate(const std::shared_ptr<BB_node>& now_BB);
+    void exit_generate(const std::shared_ptr<BB_node>& now_BB);
 };
--- source/backend/Define/backend_define.cpp
+++ source/backend/Define/backend_define.cpp
@@ -2,4 +2,17 @@
 // Created by Simon on 2022/6/2.
 //
 
-//TODO: control flow graph implementation
+#include "backend_define.h"
+#include <iostream>
+
+void BB_node::print_all(const std::shared_ptr<BB_node>& BB_head){
+    std::shared_ptr<BB_node> now = BB_head;
+    std::cout << "print BB" << std::endl;
+    return;
+}
+
+void CFG_node::print_all(const std::shared_ptr<CFG_node>& CFG_head){
+    std::shared_ptr<CFG_node> now = CFG_head;
+    std::cout << "print CFG" << std::endl;
+    return;
+}
