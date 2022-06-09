54d901b4e0821f5d496e07e44d60e2a3d1386dcf
SimonWXW <wuxiwen@mail.nwpu.edu.cn>
2022-06-10 01:15:55 +0800

    add comment to main.cpp

--- source/backend/Define/backend_define.cpp
+++ source/backend/Define/backend_define.cpp
@@ -5,16 +5,6 @@
 #include "backend_define.h"
 #include <iostream>
 
-/*
-void BBList_node::print_all(const std::shared_ptr<BBList_node>& BB_head){
-    std::shared_ptr<BBList_node> now = BB_head;
-    if(now != nullptr){
-        std::cout << "print BasicBlockList" << std::endl;
-    }
-    return;
-}
-*/
-
 void CFG_node::print_all(const std::shared_ptr<CFG_node>& CFG_head){
     std::shared_ptr<CFG_node> now = CFG_head;
     if(now != nullptr){
--- source/backend/Define/include/backend_define.h
+++ source/backend/Define/include/backend_define.h
@@ -9,21 +9,6 @@
 #include <string>
 #include <vector>
 
-/*
-typedef struct BasicBlockList_node {
-    int index = -1;
-
-    std::shared_ptr<std::vector<BasicBlockList_node>> predecessor = nullptr;
-    std::shared_ptr<std::vector<BasicBlockList_node>> successor = nullptr;
-
-    std::vector<IR_node> basic_block;
-
-    static void print_all(const std::shared_ptr<BasicBlockList_node>& BBList_head);
-} BBList_node;
-
-#define BBList_PTR std::shared_ptr<BBList_node>
- */
-
 struct CFG_node {
     int index = -1;
 
