9d6906c89f952eeaa277d07bb5f21b9b2179874e
AriharaShirone <1761944955@qq.com>
2022-06-09 18:10:12 +0800

    aa start and a little fix

--- source/backend/ActivityAnalysis/ActivityAnalysis.cpp
+++ source/backend/ActivityAnalysis/ActivityAnalysis.cpp
@@ -1,3 +1,15 @@
 //
 // Created by 17619 on 2022/6/9.
 //
+#include"backend_define.h"
+#include<string>
+#include<set>
+
+class ActivityTab{
+
+    CFG_node cfgNode;
+    std::set<std::string> ActiveVariables;
+    std::set<std::string> DeadVariables;
+
+
+};
