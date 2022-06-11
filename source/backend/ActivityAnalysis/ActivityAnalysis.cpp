b1ecb31143d658baa5cfd3c4fd661f03a74ec718
AriharaShirone <1761944955@qq.com>
2022-06-11 15:53:50 +0800

    aa bad 30%

--- source/backend/ActivityAnalysis/ActivityAnalysis.cpp
+++ source/backend/ActivityAnalysis/ActivityAnalysis.cpp
@@ -4,22 +4,41 @@
 #include"backend_define.h"
 #include<string>
 #include<set>
+#include <algorithm>
 
-class BlockVariableSets{
-    std::set<std::string> DefinedVariables;
-    std::set<std::string> UsedVariables;
-    void CalculateDefined(const CFG_node& cfgNode){
+class BlockVariableSets {
+    std::set<std::string> DefinedVariables, UsedVariables;
 
+    bool judgeOperator(std::string op) {
+        std::vector<std::string> operats = {"add", "addf", "sub", "subf", "mul", "mulf", "div", "divf", "mod"};
+        for (auto i: operats) {
+            if (op == i) return true;
+        }
+        return false;
     }
-    void CalculateUsed(const CFG_node& cfgNode){
 
+    void CalculateVariables(const CFG_node &cfgNode) {
+        for (auto i = cfgNode.basic_block.begin(); i != cfgNode.basic_block.end(); i++) {
+            if (judgeOperator(i->opera))
+                if (!((i->org_1.name == "") || (i->org_2.name == ""))) { //target cannot be empty
+                    if (UsedVariables.find(i->target.name) != UsedVariables.end() &&
+                        DefinedVariables.find(i->org_1.name) != DefinedVariables.end() &&
+                        DefinedVariables.find(i->org_1.name) != DefinedVariables.end()) {
+                        UsedVariables.insert(i->org_1.name);
+                        UsedVariables.insert(i->org_2.name);
+                        DefinedVariables.insert(i->target.name);//optimize needed,O(n^2),slow
+                    }
+                }
+        }
     }
+
 };
 
-class ActivityTab{
+class ActivityTab {
     std::set<std::string> ActiveVariables;
     std::set<std::string> DeadVariables;
-    void AnalyzeActivity(const CFG_node& cfgNode){
+
+    void AnalyzeActivity(const CFG_node &cfgNode) {
 
     }
 };
