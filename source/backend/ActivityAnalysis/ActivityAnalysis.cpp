80c277953d8b722558d8181dfe6e61dfd0182a81
AriharaShirone <1761944955@qq.com>
2022-06-12 22:50:02 +0800

    A little better? AA 30%->0->20%

--- source/backend/ActivityAnalysis/ActivityAnalysis.cpp
+++ source/backend/ActivityAnalysis/ActivityAnalysis.cpp
@@ -3,21 +3,24 @@
 //
 #include "ActivityAnalysis.h"
 
-void BlockVariableFactory::CalculateDefinedAndUsed(const CFG_node &cfgNode) {
+void BlockVariableFactory::CalculateDefinedAndUsed(CFG_node &cfgNode) {
     for (auto i = cfgNode.basic_block.begin(); i != cfgNode.basic_block.end(); i++) {
         if (judgeOperator(i->opera))
             if (!((i->org_1.name == "") || (i->org_2.name == ""))) { //target cannot be empty
                 if (cfgNode.used_variables.find(i->target.name) != cfgNode.used_variables.end() &&
-                    DefinedVariables.find(i->org_1.name) != DefinedVariables.end() &&
-                    DefinedVariables.find(i->org_1.name) != DefinedVariables.end()) {
+                    cfgNode.defined_varibles.find(i->org_1.name) != cfgNode.defined_varibles.end() &&
+                        cfgNode.defined_varibles.find(i->org_1.name) != cfgNode.defined_varibles.end()) {
                     cfgNode.used_variables.insert(i->org_1.name);
                     cfgNode.used_variables.insert(i->org_2.name);
-                    DefinedVariables.insert(i->target.name);//optimize needed,O(n^2),slow
+                    cfgNode.defined_varibles.insert(i->target.name);//optimize needed,O(n^2),slow
                 }
             }
     }
 }
 
-void CFGActivityTab::AnalyzeBlockVariables(CFG_LIST_PTR listNode) {
+void CFGActivityTab::AnalyzeBlockVariables(CFG_List_node &listHead) {
+    for(auto nowListNode= std::make_shared<CFG_List_node>(listHead);nowListNode;nowListNode=now->next){
+        CFG_node &cfgNode=*nowListNode->cfg;
 
+    }
 }
