893be097c5e93362d7a4cc53a6149ef66c9ae451
AriharaShirone <1761944955@qq.com>
2022-06-12 17:45:22 +0800

    GG

--- source/backend/ActivityAnalysis/ActivityAnalysis.cpp
+++ source/backend/ActivityAnalysis/ActivityAnalysis.cpp
@@ -3,26 +3,21 @@
 //
 #include "ActivityAnalysis.h"
 
-void BlockVariableSets::CalculateVariables(const CFG_node &cfgNode) {
+void BlockVariableFactory::CalculateDefinedAndUsed(const CFG_node &cfgNode) {
     for (auto i = cfgNode.basic_block.begin(); i != cfgNode.basic_block.end(); i++) {
         if (judgeOperator(i->opera))
             if (!((i->org_1.name == "") || (i->org_2.name == ""))) { //target cannot be empty
-                if (UsedVariables.find(i->target.name) != UsedVariables.end() &&
+                if (cfgNode.used_variables.find(i->target.name) != cfgNode.used_variables.end() &&
                     DefinedVariables.find(i->org_1.name) != DefinedVariables.end() &&
                     DefinedVariables.find(i->org_1.name) != DefinedVariables.end()) {
-                    UsedVariables.insert(i->org_1.name);
-                    UsedVariables.insert(i->org_2.name);
+                    cfgNode.used_variables.insert(i->org_1.name);
+                    cfgNode.used_variables.insert(i->org_2.name);
                     DefinedVariables.insert(i->target.name);//optimize needed,O(n^2),slow
                 }
             }
     }
 }
 
+void CFGActivityTab::AnalyzeBlockVariables(CFG_LIST_PTR listNode) {
 
-void CFGActivityTab::AnalyzeBlockVariables() {
-    CFG_List_node node;
-    blockVariableSets.CalculateVariables(*node.cfg);
-    for(){
-
-    }
 }
