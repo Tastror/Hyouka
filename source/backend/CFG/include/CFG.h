aa3c4d41af06dbe9b6d975ce16e7b9bd6242a7b0
SimonWXW <wuxiwen@mail.nwpu.edu.cn>
2022-06-13 02:43:03 +0800

    LINEAR 50%

--- source/backend/CFG/include/CFG.h
+++ source/backend/CFG/include/CFG.h
@@ -8,10 +8,6 @@
 
 class CFG{
 public:
-    CFG_PTR head;
-    CFG_PTR now_cfg;
-    CFG_PTR tail;
-    IR_PTR IR;
     static int line_num;
     static int basic_block_num;
 
@@ -22,22 +18,21 @@ public:
     IR_node find_successor(const std::shared_ptr<IR_node>& target_IR);
     bool is_exist_basic_block(const std::shared_ptr<IR_node>& target_IR);
 
-    void Generate();
-
     void cfg_generate(const std::shared_ptr<IR_node>& now_IR);
 };
 
 class CFG_LIST{
 public:
+    std::string head;
+    IR_PTR IR;
     static int cfg_num;
     std::unordered_map<std::string, CFG_PTR> cfg_list;
 
-    explicit CFG_LIST(const std::shared_ptr<CFG_List_node>& CFG_head);
+    explicit CFG_LIST(const std::shared_ptr<IR_node>& IR_head);
 
     void create_empty_cfg_list();
-    void create_cfg(const std::shared_ptr<CFG_List_node>& now_CFG);
 
     void Generate();
 
-    void cfg_list_generate(const std::shared_ptr<CFG_List_node>& now_CFG);
+    void cfg_list_generate(const std::shared_ptr<IR_node>& now_IR);
 };
