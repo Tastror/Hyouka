75ea7dd7755104ebea5020e9a02894a1ab7c9620
AriharaShirone <1761944955@qq.com>
2022-06-14 02:17:35 +0800

    ActivityAnalysis Almost Done

--- source/backend/ActivityAnalysis/include/ActivityAnalysis.h
+++ source/backend/ActivityAnalysis/include/ActivityAnalysis.h
@@ -8,21 +8,38 @@
 #include <algorithm>
 
 class BlockVariableFactory {
-    static bool judgeOperator(std::string op) {
-        std::vector<std::string> operats = {"add", "addf", "sub", "subf", "mul", "mulf", "div", "divf", "mod"};
-        for (auto i: operats) {
-            if (op == i) return true;
-        }
-        return false;
+
+    static bool judgeOperator(const std::string &op) {
+        const std::vector<std::string> operats = {"add", "addf", "sub", "subf", "mul", "mulf", "div", "divf", "mod",
+                                                  "assign"};
+        return std::any_of(operats.begin(), operats.end(), [&op](const std::string &str) {
+            return str == op;
+        });
     }
+
 public:
-    //std::set<std::string> DefinedVariables, usedVariables;
+    static std::map<int, bool> CFGChange;
+
     static void CalculateDefinedAndUsed(CFG_node &cfgNode);
+
+    static void CalculateActivity(CFG_node &cfgNode);
+
+    static bool DetectChange() {
+        if (CFGChange.empty()) {
+            return true;
+        } else {
+            return std::any_of(CFGChange.begin(), CFGChange.end(), [](const std::pair<int, bool> &item) -> auto {
+                return item.second;
+            });
+        }
+
+    }
 };
 
 class CFGActivityTab {
 public:
-    void AnalyzeBlockVariables(CFG_List_node &listHead);
-    void Generate();
+    void AnalyzeBlockVariables(std::vector<CFG_PTR > &cfglist);
+
+    static void print_all(const std::vector<std::shared_ptr<CFG_node>> &vector);
 };
 
