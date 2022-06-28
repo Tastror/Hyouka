0f98207c8a2dcc80ad89ad6c1c4745607cc37681
SimonWXW <wuxiwen@mail.nwpu.edu.cn>
2022-06-29 00:02:59 +0800

    still thinking how to traverse register allocated cfg to generate arm

--- source/backend/InstructionAllocate/include/InstructionAllocate.h
+++ source/backend/InstructionAllocate/include/InstructionAllocate.h
@@ -9,8 +9,15 @@
 class InstructionAllocator {
 public:
 
-    std::vector<CFG_pro_PTR> CFG_pro_blocks_chain;
+    std::map<std::string, std::vector<CFG_pro_PTR>> CFG_map;
     ARM_code_vec ARM_code;
-    explicit InstructionAllocator(const std::map<std::string, std::vector<CFG_pro_PTR>>& CFG_pro_function_chain) {}
+
+    explicit InstructionAllocator(const std::map<std::string, std::vector<CFG_pro_PTR>>& CFG_pro_function_chain);
+
     void Generate();
+
+    void program_generate(const std::shared_ptr<CFG_pro_node>& now_CFG);
+    void global_generate(const std::shared_ptr<CFG_pro_node>& now_CFG);     //global variables
+    void function_generate(const std::shared_ptr<CFG_pro_node>& now_CFG);
+
 };
