54e80aba7395a9a1aae4fdf57f259c2ea4070b44
SimonWXW <wuxiwen@mail.nwpu.edu.cn>
2022-06-29 00:16:47 +0800

    arm 5%

--- source/backend/InstructionAllocate/include/InstructionAllocate.h
+++ source/backend/InstructionAllocate/include/InstructionAllocate.h
@@ -16,8 +16,8 @@ public:
 
     void Generate();
 
-    void program_generate(const std::shared_ptr<CFG_pro_node>& now_CFG);
+    void program_generate(const std::map<std::string, std::vector<CFG_pro_PTR>>& CFG_pro_function_chain);
     void global_generate(const std::shared_ptr<CFG_pro_node>& now_CFG);     //global variables
-    void function_generate(const std::shared_ptr<CFG_pro_node>& now_CFG);
+    void function_generate(const std::vector<std::shared_ptr<CFG_pro_node>>& now_CFG);
 
 };
