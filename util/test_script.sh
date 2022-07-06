3749209122ad5d1ad85b978f36b2f5723bd308ce
SimonWXW <wuxiwen@mail.nwpu.edu.cn>
2022-07-06 19:13:19 +0800

    modify test_script.sh

--- util/test_script.sh
+++ util/test_script.sh
@@ -1,18 +1,19 @@
-COMPILER='.ild/SysY'
-TEST_PATH='./test'
-passed=0
+COMPILER='../cmake-build-debug/compiler'
+TEST_PATH='../testcase/functional'
+LIB_PATH = '../runtime_lib'
+AC=0
 CE=0
 WA=0
-for source_file in $(ls $(find $TEST_PATH -name '*.c'))
+for source_file in $(ls $(find $TEST_PATH -name '*.sy'))
 do
 	if [ ${source_file%.*} == $TEST_PATH/sylib ];
 	then
 		continue
 	fi
-	$COMPILER ${source_file%.*}.c > ${source_file%.*}.s
+	$COMPILER ${source_file%.*}.sy > ${source_file%.*}.s
 	if [ $? == 0 ];
 	then
-		arm-none-linux-gnueabihf-gcc ${source_file%.*}.s $TEST_PATH/sylib.c -o ${source_file%.*}.tmp -mcpu=cortex-a72 --static
+		arm-linux-gnueabihf-gcc -static $LIB_PATH/libsysy.a ${source_file%.*}.s -o ${source_file%.*}.tmp
 		if [ -f ${source_file%.*}.in ];
 		then
 			stdout=$(qemu-arm ${source_file%.*}.tmp < ${source_file%.*}.in)
@@ -22,25 +23,25 @@ do
 		retval=$?
 		if [ -n "$stdout" ];
 		then
-			echo -e "$stdout" > ${source_file%.*}.out
-			echo $retval >> ${source_file%.*}.out
+			echo -e "$stdout" > ${source_file%.*}.ans
+			echo $retval >> ${source_file%.*}.ans
 		else
-			echo $retval > ${source_file%.*}.out
+			echo $retval > ${source_file%.*}.ans
 		fi
-		result=$(cat ${source_file%.*}.out | tr "\n" " ")
-		answer=$(cat ${source_file%.*}.ans | tr "\n" " ")
+		result=$(cat ${source_file%.*}.ans | tr "\n" " ")
+		answer=$(cat ${source_file%.*}.out | tr "\n" " ")
 		cmp -s ${source_file%.*}.out ${source_file%.*}.ans
 		if [ $? == 0 ];
 		then
-			printf "\e[32m%-20s testcase: %-30s (result: %-10s | answer: %-10s).\e[0m\n" "[PASSED]" ${source_file%.*} "$result" "$answer"
-			passed=$((passed + 1))
+			printf "\e[32m%-20s testcase: %-30s (result: %-10s | answer: %-10s).\e[0m\n" "[Accepted]" ${source_file%.*} "$result" "$answer"
+			AC=$((AC + 1))
 		else
-			printf "\e[31m%-20s testcase: %-30s (result: %-10s | answer: %-10s).\e[0m\n" "[WRONG ANSWER]" ${source_file%.*} "$result" "$answer"
+			printf "\e[31m%-20s testcase: %-30s (result: %-10s | answer: %-10s).\e[0m\n" "[Wrong Answer]" ${source_file%.*} "$result" "$answer"
 			WA=$((WA + 1))
 		fi
 	else
-		printf "\e[33m%-20s testcase: %-30s compilation failed.\e[0m\n" "[COMPILE ERROR]" ${source_file%.*}
+		printf "\e[33m%-20s testcase: %-30s compilation failed.\e[0m\n" "[Compile Error]" ${source_file%.*}
 		CE=$((CE + 1))
 	fi
 done
-echo -e "\e[32mpassed: $passed/$((passed + WA + CE))          \e[31mWA: $WA/$((passed + WA + CE))          \e[33mCE: $CE/$((passed + WA + CE))\e\e[0m"
+echo -e "\e[32mAC: $AC/$((AC + WA + CE))          \e[31mWA: $WA/$((AC + WA + CE))          \e[33mCE: $CE/$((AC + WA + CE))\e\e[0m"
